011333f3e570e5c12b2e0e276bb38d49dc414513
Tjaz <25076630+TjazBuilder@users.noreply.github.com>
2018-08-23 13:40:53 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 An API that returns data from the Gizmo website.
+
+## Installation ##
+
+    npm install gizmo-api
+
+## Usage ##
+
+    const gizmo = require("gizmo-api");
+
+### To get JSON output for a user ###
+
+    gizmo.getUser(query);
+
+The query can either be an integer (UserID) or string (Username).
---
+++ index.js
@@ -0,0 +1,28 @@
+const xmlRequest = require("xmlhttprequest");
+
+function xmlMakeRequest(url, callback) {
+
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    var xml = new XMLHttpRequest();
+    xml.open("GET", url, true);
+    
+    xml.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            json = JSON.parse(this.responseText);
+            if (typeof json === undefined) {
+                callback("Failed to parse JSON response");
+            } else {
+                callback(json);
+            }
+        }
+    };
+
+    xml.send();
+}
+
+exports.getUser = function(userQuery) {
+    xmlMakeRequest("https://gizmogames.000webhostapp.com/api/user?=" + userQuery, continueUser);
+    function continueUser(xmlResponse) {
+        return xmlResponse;
+    }
+}
