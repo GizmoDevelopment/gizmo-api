a0fffc1b2e3990b3d78e3dce15657bece3765a19
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-06-21 11:31:36 +0200

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,36 @@
+export type SearchQuery = number | string;
+
+export interface BaseResult {
+	type: "success" | "error";
+}
+
+export interface SuccessResult<T> extends BaseResult {
+	type: "success";
+	data: T;
+}
+
+export interface ErrorResult extends BaseResult {
+	type: "error";
+	message: string;
+}
+
+export interface GizmoFetchResponse<T> {
+	status: number;
+	data: SuccessResult<T> | ErrorResult;
+}
+
+export interface User {
+	id: number;
+	username: string;
+	badges: Badge[];
+	avatar_url: string;
+	header_url: string;
+	about: string;
+	created: number;
+}
+
+export interface AuthenticatedUser extends User {
+	token: string;
+}
+
+export type Badge = "DEVELOPER" | "MODERATOR";
