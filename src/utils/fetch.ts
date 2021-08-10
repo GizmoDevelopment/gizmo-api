1a8bc23ba4497d6a0e8b36066b8d34911ac60df7
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-08-10 18:21:09 +0200

    properly polyfill cross-fetch

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -4,16 +4,18 @@ import { GIZMO_ENDPOINT } from "@utils/constants";
 // Types
 import { GizmoFetchResponse } from "@typings";
 
-// Variables
-let fetch = window.fetch;
-
-export async function gizmoFetch <T> (uri: string, options?: RequestInit): Promise<GizmoFetchResponse<T>> {
-
-	if (typeof fetch !== "function") {
-		fetch = require("cross-fetch");
+function _fetch (url: string, options: RequestInit): Promise<Response> {
+	if (typeof fetch === "function") {
+		return fetch(url, options);
+	} else {
+		// eslint-disable-next-line @typescript-eslint/no-var-requires
+		return require("cross-fetch")(url, options);
 	}
+}
 
-	const response = await fetch(`${GIZMO_ENDPOINT}${uri}`, options);
+export async function gizmoFetch <T> (uri: string, options: RequestInit = {}): Promise<GizmoFetchResponse<T>> {
+
+	const response = await _fetch(`${GIZMO_ENDPOINT}${uri}`, options);
 
 	return {
 		status: response.status,
