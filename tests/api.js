3b4b571125dde784897d2ba78850296cd130e794
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-08-10 18:27:50 +0200

    test script

---
+++ tests/api.js
@@ -0,0 +1,21 @@
+// Modules
+const assert = require("assert");
+const gizmo = require("../lib");
+
+describe("gizmo-api test", () => {
+
+	it("Should be able to search for users", async () => {
+
+		const user = await gizmo.searchForUser("Tja");
+
+		assert.strictEqual(user.id, 1);
+	});
+
+	it("Should be able to fetch user by ID", async () => {
+
+		const user = await gizmo.getUserById(1);
+
+		assert.strictEqual(user.id, 1);
+	});
+
+});
