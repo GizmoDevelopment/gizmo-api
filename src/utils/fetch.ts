a81181c77a5a24ad7928527504ad0fdc579390f3
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-08-10 16:01:41 +0200

    conditionally import cross-fetch

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,13 +1,17 @@
-// Modules
-import fetch from "cross-fetch";
-
 // Utils
 import { GIZMO_ENDPOINT } from "./constants";
 
 // Types
 import { GizmoFetchResponse } from "../types";
 
-export async function gizmoFetch<T> (uri: string, options?: RequestInit): Promise<GizmoFetchResponse<T>> {
+// Variables
+let fetch = window.fetch;
+
+export async function gizmoFetch <T> (uri: string, options?: RequestInit): Promise<GizmoFetchResponse<T>> {
+
+	if (typeof fetch !== "function") {
+		fetch = require("cross-fetch");
+	}
 
 	const response = await fetch(`${GIZMO_ENDPOINT}${uri}`, options);
 
