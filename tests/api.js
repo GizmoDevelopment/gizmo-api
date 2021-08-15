b4289e590492a5a92fec008e40b03660191e8614
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-08-15 17:00:14 +0200

    test user properties

--- tests/api.js
+++ tests/api.js
@@ -18,4 +18,17 @@ describe("gizmo-api test", () => {
 		assert.strictEqual(user.id, 1);
 	});
 
+	it("Should be able to read basic user information", async () => {
+
+		const user = await gizmo.getUserById(1);
+
+		assert.strictEqual(typeof user.id, "number");
+		assert.strictEqual(typeof user.username, "string");
+		assert.strictEqual(typeof user.badges.length, "number");
+		assert.strictEqual(typeof user.avatar_url, "string");
+		assert.strictEqual(user.hasOwnProperty("banner_url"), true);
+		assert.strictEqual(typeof user.about_me, "string");
+		assert.strictEqual(typeof user.created, "number");
+	});
+
 });
